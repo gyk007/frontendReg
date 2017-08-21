1c999a1bf1bb4aabdf5a25126ff794a66bef3fc9
Andrey Zhuk <azhuk@alaskartech.com>
2017-08-21 13:15:09 +0300

    Conf

--- admin/src/conf/conf.js
+++
@@ -1,9 +0,0 @@
-const conf = {
-	url: {
-		catalog  : 'http://localhost:9888/alko/catalog/',
-		category : 'http://localhost:9888/alko/catalog/category/',
-		clients  : 'http://localhost:9888/alko/client/',
-	}
-}
-
-export default conf
