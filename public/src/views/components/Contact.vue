29f59b3a481aaf64a266cd1dbcf7b70096085c3a
Andrey Zhuk <azhuk@alaskartech.com>
2017-08-23 19:29:17 +0300

    Правки конфликотв

---
+++ public/src/views/components/Contact.vue
@@ -0,0 +1,40 @@
+<template>
+	<section class="popup popup__category" id="popup__contact">
+
+
+	<div class="popup__hdr">Обратная связь</div>
+
+	<div class="popup__category-form">
+		<div class="popup__product-form--holder">
+		<label>
+			<span style="font-size: 15px">Заголовок</span>
+			<input class="input"/>
+		</label>
+		</div>
+		<div class="popup__product-form--holder">
+		<label>
+			<span style="font-size: 15px">Ваш вопрос</span>
+			<textarea></textarea>
+		</label>
+		</div>
+
+		<button class="btn" v-on:click="sent">Отправить</button>
+
+	</div>
+</section>
+</template>
+
+
+<script>
+  import store from '../../store/catalog.js'
+  import $      from 'jquery'
+
+  export default {
+	methods: {
+		sent() {
+			$.fancybox.close()
+		},
+	},
+  }
+</script>
+
